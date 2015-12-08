f4b274ad57d0df90e93279f3bafd51eea39c1300
KimberleyLawrence <kimberleynedic@gmail.com>
2015-12-07 19:42:26 -0800

    Adding new expression and changing title string

--- MainController.js
+++ MainController.js
@@ -1,5 +1,7 @@
 // creating a new controller called MainController, a controller manages the app's data. In this we use the property title to store a string and attach a scope
 app.controller('MainController',
                ['$scope', function($scope) {
-                 $scope.title = 'Top Sellers in Books';
+                 $scope.title = 'My own string';
+                 //adding new property - promo
+                 $scope.promo = 'Promos own string';
                }]);
