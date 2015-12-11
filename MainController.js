545e5c9af6d9c4bc7bc3208d0069e3171b21eaab
KimberleyLawrence <kimberleynedic@gmail.com>
2015-12-10 18:53:32 -0800

    Adding new products and ng-repeat
    REVIEW
    A module contains the different components of an AngularJS app
    A controller manages the app's data
    An expression displays values on the page
    A filter formats the value of an expression

--- MainController.js
+++ MainController.js
@@ -4,10 +4,18 @@ app.controller('MainController',
                  $scope.title = 'My own string';
                  //adding new property - promo
                  $scope.promo = 'Promos own string';
-                 $scope.product= {
-                   name: 'The life of Trees',
-                   price: 19,
-                   //adding in a new property year, month, date
-                   pubdate: new Date('2014', '03', '08')
-                 }
+                 $scope.products = [
+   	 {
+      name: 'The Book of Trees',
+      price: 19,
+      pubdate: new Date ('2014', '03', '08'),
+      cover: 'img/the-book-of-trees.jpg'
+  	  },
+   	 {
+      name: 'Program or be Programmed',
+   	  price: 8,
+      pubdate: new Date('2013', '08', '01'),
+      cover: 'img/program-or-be-programmed.jpg'
+  		}
+    ]
                }]);
