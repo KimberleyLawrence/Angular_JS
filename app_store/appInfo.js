46ba7792891f29580169d5623e32e0823bab76d3
KimberleyLawrence <kimberleynedic@gmail.com>
2015-12-15 14:13:47 -0800

    Adding in notes on latest changes

--- app_store/appInfo.js
+++ app_store/appInfo.js
@@ -1,3 +1,7 @@
+// creating a new directive to teach the browser of a new html element. app.directive created a new directive named appInfo. It returns with 3 options:
+//restrict - specifies how the directive will be used in the view, the 'E' means it will be used as a new HTML element
+//scope - specifies that we will pass information into theis directive through an attribute named info. The = tells the directive to look for an attribute named info and the app-info element, like this: <app-info info="shutterbug"></app-info>. The data in info becomes available to use in the template given by templateURL
+//templateURL specifies the HTML to use in order to display the data in scope.info. found in appInfo.html
 app.directive('appInfo', function() {
   return {
     restrict: 'E',
