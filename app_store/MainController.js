02304d42907e7f5a26b55ea5db732bb6c602194f
KimberleyLawrence <kimberleynedic@gmail.com>
2015-12-15 13:45:59 -0800

    Creating a new store app

---
+++ app_store/MainController.js
@@ -0,0 +1,23 @@
+// three objects are controlled in here, .move, .shutterbug, .gameboard, all that contain prices and titles
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.move = {
+    icon: 'img/move.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  };
+
+  $scope.shutterbugg = {
+    icon: 'img/shutterbugg.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  };
+
+  $scope.gameboard = {
+    icon: 'img/gameboard.jpg',
+    title: 'Gameboard',
+    developer: 'Armando P.',
+    price: 1.99
+  };
+}]);
