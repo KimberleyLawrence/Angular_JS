aca97de3fbf0fe95265f18a2b28eada7c72eefa7
KimberleyLawrence <kimberleynedic@gmail.com>
2015-12-16 20:57:10 -0800

    Adding a new property with a list of items and condensing code to run the new apps added

--- app_store/MainController.js
+++ app_store/MainController.js
@@ -26,4 +26,32 @@ app.controller('MainController', ['$scope', function($scope) {
     developer: 'Forecast',
     price: 1.99
   };
+  // new property
+ $scope.apps = [
+   {
+     icon: 'img/move.jpg',
+     title: 'MOVE',
+     developer: 'MOVE, Inc.',
+     price: 0.99
+   },
+   {
+     icon: 'img/shutterbugg.jpg',
+     title: 'Shutterbugg',
+     developer: 'Chico Dusty',
+     price: 2.99
+   },
+   //adding in 2 more objects of my own choosing
+   {
+     icon: 'http://www.lavasoft.com/mylavasoft/sites/default/files/official-pinterest-logo-tile-300x300.img_assist_custom-179x179.png',
+     title: 'Pinterest',
+     developer: 'Pinterest Guys',
+     price: 0.10
+   },
+   {
+     icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png',
+     title: 'LinkedIn',
+     developer: 'Awesome peeps at LinkedIn',
+     price: 0.00
+   }
+   ]
 }]);
