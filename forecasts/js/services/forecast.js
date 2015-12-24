ccf138fe0c59c2dbde41ab8203b0b0ed5314a901
KimberleyLawrence <kimberleynedic@gmail.com>
2015-12-24 22:11:28 +1100

    Explaining a new service

--- forecasts/js/services/forecast.js
+++ forecasts/js/services/forecast.js
@@ -1,3 +1,4 @@
+// this made a new service - using app.factory to create a new service name called forecast
 app.factory('forecast', ['$http', function($http) {
   return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
             .success(function(data) {
@@ -7,4 +8,3 @@ app.factory('forecast', ['$http', function($http) {
               return err;
             });
 }]);
-                           
