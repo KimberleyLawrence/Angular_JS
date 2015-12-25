84a9b0c41bcb60721a6a1be9022d21e63f65c4f5
KimberleyLawrence <kimberleynedic@gmail.com>
2015-12-25 22:41:19 +1100

    Explaining the code earlier.

--- forecasts/js/controllers/MainController.js
+++ forecasts/js/controllers/MainController.js
@@ -1,3 +1,4 @@
+// in the controller, we need to use the forecast service to fetch data from the server. First we add forecast into maincontroller as a dependency so that it is available for use. Then within the controller we used forecast to asynchronously fetch the weather data from the server and store it into $scope.fiveDay
 app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {
   forecast.success(function(data){
     $scope.fiveDay = data;
--- forecasts/js/services/forecast.js
+++ forecasts/js/services/forecast.js
@@ -1,5 +1,7 @@
-// this made a new service - using app.factory to create a new service name called forecast
+// creating a new service
+// the forecast service needs to use AngularJS built in $http to fetch JSON from the server, therefore we add $http to the forecast as a dependency [ 'http', function($http).. now http is available to use inside forecast
 app.factory('forecast', ['$http', function($http) {
+  // inside we use http.get to request the weather data, if successful the weather data is returned, otherwise an error is returned
   return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
             .success(function(data) {
               return data;
@@ -8,3 +10,4 @@ app.factory('forecast', ['$http', function($http) {
               return err;
             });
 }]);
+             
