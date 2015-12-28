684ab9f45d95e7f86e0316e5f72921698c896564
KimberleyLawrence <kimberleynedic@gmail.com>
2015-12-29 09:34:40 +1100

    Explaining what was created in previous exercise
    In app.js inside the app.config() method, we use Angular's
    $routeProvider to define the application routes.
    We used .when() to map the URL / to to the controller HomeController
    and the template home.html. The HomeController uses the service
    js/services/photos.js to fetch the array of all photos from
    https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/phot
    os.json and stores it into $scope.photos. The home.html uses ng-repeat
    to loop through each item in the photos array and display each photo.
    Otherwise if a user accidentally visits a URL other than /, we just
    redirect to / using .otherwise().
    Now when a user visits /, a view will be constructed by injecting
    home.html into the <div ng-view></div> in index.html.

--- expanded_app/app.js
+++ expanded_app/app.js
@@ -1,4 +1,18 @@
 var app = angular.module('GalleryApp', ['ngRoute']);
+//using the app.config method we use $routeProvider to define the apps routes
+app.config(function ($routeProvider) {
+  $routeProvider
+  // we use .when to map the url= / to the controller HomeController and the template home.html. The homeController uses the service js/service/photos.js to fetch the array of all photos from a link and stores in $scope.photos. The home.html uses ng-repeat to then loop through the items in the photo array and display them
+    .when('/', {
+      controller: 'HomeController',
+      templateUrl: 'views/home.html'
+    })
+  // Otherwise of a user accidentally visits a URL other than / they are redirected to / using .otherwise()
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+//Not when a user visits /, a view will be contructed by injecting home.html into the <div ng-view></div> in index.htmlvar app = angular.module('GalleryApp', ['ngRoute']);
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
